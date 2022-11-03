81eb95dbe5d49b8a4a54749cdd7290bc688734af
Kong-16 <alsgur97@naver.com>
2022-11-03 21:11:26 +0900

    -

--- src/components/views/SearchBar/SearchBar.js
+++ src/components/views/SearchBar/SearchBar.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react'
 import StockItem from '../Data/Stocks.json';
 import TextField from '@mui/material/TextField';
-import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete'
+import Autocomplete from '@mui/material/Autocomplete'
 
 
 function SearchBar() {
-    const [query, setQuery] = useState("");
-    // const search = (data) => {
-    //     return data.filter((item) => keys.some((key) => item[key].startsWith(query)));
-    // };
+    const keys = ['code', 'name']
+    const [query, setQuery] = useState('');
+    const filteroption = (options, { inputValue }) => {
+        return options.filter((item) => keys.some((key) => item[key].startsWith(inputValue)))
+    }
+
     return (
         <Autocomplete
             id="search_bar"
+            autoComplete
+            includeInputInList
+            onChange={(event, selectedValue) => setQuery(selectedValue)}
             inputValue={query}
             onInputChange={(event, inputValue) => setQuery(inputValue)}
             options={StockItem}
             getOptionLabel={(option) => option.name + '  ' + option.code}
-            filterOptions={(options, { inputValue }) => options.filter(item => item.name.startsWith(inputValue) || item.code.startsWith(inputValue))}
+            filterOptions={filteroption}
             noOptionsText={'찾으려는 주식이 없습니다...'}
             sx={{ minWidth: 300 }}
-            renderInput={(params) => <TextField {...params} label="Search" />}
+            renderInput={(params) => <TextField
+                {...params}
+                label="Search" />}
         />
     )
 }
